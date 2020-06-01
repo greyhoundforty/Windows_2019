442bcbfe8515181112f38daa7f5a811f82e8f154
greyhoundforty <ryantiffany@fastmail.com>
2020-06-01 12:41:45 -0500

    Adding in provider API info
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+variable "iaas_classic_username" {
+  description = "IaaS username."
+  type = string
+  default = ""  
+}
+
+variable "iaas_classic_api_key" {
+  description = "IaaS API Key."
+  type = string
+  default = ""
+}
+
 variable "datacenter" {
   description = "Default datacenter to deploy in to."
   type = string
